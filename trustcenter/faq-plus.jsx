ad91e10306cb442da6af9b4db658b6421e115619
maailili <maailili@outlook.com>
2016-06-07 15:39:05 +0800

    update

--- trustcenter/faq-plus.jsx
+++ trustcenter/faq-plus.jsx
@@ -2,5 +2,12 @@
      $(".content-faq-footer").hide();
      $(".content-faq label").click(function(){
          $(this).siblings().toggle();
+         if($(this).siblings() == 'none'){
+            $(this).parent(".content-faq").removeClass("content-faqys");
+            $(this).removeClass("faq-lan");
+         }else{
+            $(this).parent(".content-faq").addClass("content-faqys");
+            $(this).addClass("faq-lan");
+         }
      });
  });
