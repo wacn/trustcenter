f02d7bf6349884b96c494719606cae13c1d89799
maailili <maailili@outlook.com>
2016-07-11 11:38:46 +0800

    update

--- trustcenter/faq-plus.jsx
+++ trustcenter/faq-plus.jsx
@@ -13,13 +13,12 @@
     	},function(){
           		$(this).css({"color":"#00a8d9"});
     	});
-    	
-    	function enUs(a){
-     	var  current = "../.." + (window.location.pathname.indexOf("en-us")==-1?"/zh-cn":window.location.pathname.replace("en-us","zh-cn"));
-     		$(a).attr("href",current);
-    	};
-    	function zhCn(b){
-     		    current = "../.." + (window.location.pathname.indexOf("zh-cn")==-1?"/en-us":window.location.pathname.replace("zh-cn","en-us"));
-     		$(b).attr("href",current);
+    	funtion enUs(){
+     	var  currentEnUs = "../.." + (window.location.pathname.indexOf("en-us")==-1?"/zh-cn":window.location.pathname.replace("en-us","zh-cn"));
+      window.location.href= currentEnUs;
+    	}    
+    	function zhCn(){
+     		    currentZhCn = "../.." + (window.location.pathname.indexOf("zh-cn")==-1?"/en-us":window.location.pathname.replace("zh-cn","en-us"));
+     	     	window.location.href= currentZhCn;
     	};
  });
