3450036f368ee4d91aa54233f78681b796ba794e
maailili <maailili@outlook.com>
2016-07-11 11:12:46 +0800

    update

--- trustcenter/faq-plus.jsx
+++ trustcenter/faq-plus.jsx
@@ -14,12 +14,12 @@
           		$(this).css({"color":"#00a8d9"});
     	});
     	
-    	$("#zh-cn").click(function(){
+    	function enUs(a){
      	var  current = "../.." + (window.location.pathname.indexOf("en-us")==-1?"/zh-cn":window.location.pathname.replace("en-us","zh-cn"));
-     		$(this).children("a").attr("href",current);
-    	});
-    		$("#en-us").click(function(){
+     		$(a).attr("href",current);
+    	};
+    	function zhCn(b){
      		    current = "../.." + (window.location.pathname.indexOf("zh-cn")==-1?"/en-us":window.location.pathname.replace("zh-cn","en-us"));
-     		$(this).children("a").attr("href",current);
-    	});
+     		$(b).attr("href",current);
+    	};
  });
